6e2717313e2cb80f167069d68a1090c08fae2dc3
jimmg35 <jim60308@gmail.com>
2024-06-11 13:42:46 +0800

    lint

--- src/container/ArticlesContainer/Search/index.tsx
+++ src/container/ArticlesContainer/Search/index.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import ArticleChip from '@/component/ArticleChip'
 import Input from '@/component/Input'
 import algoliasearch from 'algoliasearch/lite'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import React from 'react'
-import Article from '../Gallery/Article'
 
 console.log(process.env.NEXT_PUBLIC_ALGOLIA_APP_ID)
 console.log(process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY)
@@ -15,6 +13,7 @@ const searchClient = algoliasearch(
   'latency',
   '4cb45a06804396bc2e1ca81137fdfca1'
 )
+console.log(searchClient)
 
 const Search = () => {
   return (
@@ -23,7 +22,7 @@ const Search = () => {
         icon={<MagnifyingGlassIcon className="w-5 h-5" />}
         placeholder="search anything..."
         value=""
-        onChange={(value) => {}}
+        onChange={() => {}}
       />
     </div>
   )
